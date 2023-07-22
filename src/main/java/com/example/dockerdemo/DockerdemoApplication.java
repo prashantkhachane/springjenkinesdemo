c53eb5f0711fc160f1469564790581fc409b3243
prashant <khacpr@mahindra.com>
2023-07-22 22:04:41 +0530

    First commit

---
+++ src/main/java/com/example/dockerdemo/DockerdemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerdemoApplication.class, args);
+	}
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to docker demo...!";
+	}
+
+}
